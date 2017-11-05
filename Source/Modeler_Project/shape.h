4f6027b56a4aded76b8712292f42cd1e5ac43150
Mike <michael.yelverton89@gmail.com>
2017-11-05 13:18:36 -0800

    Latest Update

--- Source/Modeler_Project/shape.h
+++ Source/Modeler_Project/shape.h
@@ -10,7 +10,8 @@ class Shape
 {
 public:
     enum shape {Line, Polyline, Polygon, Rectangle, Square, Ellipse, Circle, Text};
-    Shape(QPainterDevice *device=nullptr,nid=-1,Shape::shape s=Line):painter(device),id(nid),objShape(s){}
+    Shape(QPainterDevice *device=nullptr,nid=-1,Shape::shape s=Line, QPen npen, QBrush nbrush):
+        painter(device),id(nid),objShape(s),pen(npen),brush(nbrush){}
     int setId(int nid)
     {
        id = nid;
@@ -57,7 +58,6 @@ protected:
     {
         return painter;
     }
-
 private:
     QPainter painter;
     int id;
@@ -69,8 +69,8 @@ private:
 class Line : public Shape
 {
 public:
-    Line():Shape(),start(QPoint(0,0)),end(QPoint(0,0)){}
-    Line(QPoint s,QPoint e):Shape(),start(s),end(e){}
+    Line(QPainterDevice* device=nullptr,int nid=-1, QPen npen, QBrush nbrush,QPoint s=(QPoint(0,0)), QPoint e=(QPoint(0,0)))
+        :Shape(device,nid,Shape::shape::Line,npen,nbrush),start(s),end(e){}
     void draw(QPaintDevice* device)
     {
         QPainter pnt = getPainter();
@@ -91,11 +91,9 @@ private:
 class Polyline : public Shape
 {
 public:
-    Polyline():Shape(), p(p.push_back(QPoint(0,0))){}
-    Polyline(vector<QPoint> &source):Shape()
-    {
-         p = source;
-    }
+    Polyline(QPaintDevice* device=nullptr,int nid=-1,QPen npen,QBrush nbrush,const vector<QPoint> &source):
+        Shape(device,nid,Shape::shape::Polyline,npen,nbrush){p = source;}
+
     void draw(QPaintDevice *device)
     {
          QPainter pnt = getPainter();
@@ -115,11 +113,9 @@ private:
 class Polygon : public Shape
 {
 public:
-    Polygon():Shape(), p(p.push_back(QPoint(0,0))){}
-    Polygon(vector<QPoint> &source):Shape()
-    {
-        p = source;
-    }
+    Polygon(QPainterDevice* device,int nid=-1,QPen npen,QBrush nbrush,const vector<QPoint> &source):
+        Shape(device,nid,Shape::shape::Polygon,npen,nbrush){p = source;}
+
     void draw(QPaintDevice *device)
     {
         QPainter pnt = getPainter();
@@ -153,33 +149,160 @@ private:
 class Rectangle : public Shape
 {
 public:
-
+    Rectangle(QPaintDevice* device,int nid=-1,Shape::shape s=Rectangle,QPen npen,QBrush nbrush,QPoint nUL,int nw,int nh):
+        Shape(device,nid,s,npen,nbrush),upperLeft(nUL),width(nw),height(nh){}
+    void draw()
+    {
+        QPainter pnt = getPainter();
+        pnt.setPen(this->getPen());
+        pnt.setBrush(this->getbrush());
+        pnt.drawRect((getUpperLeft()).x(),(getUpperLeft()).y(),getWidth(),getHeight());
+    }
+    void move(QPoint nUL,int nw,int nh)
+    {
+        setUpperLeft(nUL);
+        setWidth(nw);
+        setHeight(nh);
+    }
+    double calcPerimeter()
+    {
+        return ((getWidth()*2)+(getHeight()*2));
+    }
+    double calcArea()
+    {
+        return (getWidth()*getHeight());
+    }
+    void setUpperLeft(QPoint nUL)
+    {
+        upperLeft = nUL;
+    }
+    void setWidth(int nw)
+    {
+        width = nw;
+    }
+    void setHeight(int nh)
+    {
+        height = nh;
+    }
+    QPoint& getUpperLeft()const
+    {
+        return upperLeft;
+    }
+    int getWidth()const
+    {
+        return width;
+    }
+    int getHeight()const
+    {
+        return height;
+    }
 private:
-
+    QPoint upperLeft;
+    int width, height;
 };
 
 class Square : public Rectangle
 {
 public:
-
+    Square(QPaintDevice* device,int nid=-1,QPen npen,QBrush nbrush,QPoint nUL,int nside):
+        Rectangle(device,nid,Shape::shape::Square,npen,nbrush,nUL,nside,nside){}
+    void draw()
+    {
+        this->Rectangle::draw();
+    }
+    void move(QPoint np,int ns)
+    {
+        this->Rectangle::move(np,ns,ns);
+    }
+    double calcPerimeter()
+    {
+        this->Rectangle::calcPerimeter();
+    }
+    double calcArea()
+    {
+        this->Rectangle::calcArea();
+    }
 private:
-
 };
 
 class Ellipse : public Shape
 {
 public:
+    Ellipse(QPaintDevice* device,int nid=-1,Shape::shape s=Ellipse,QPen npen,QBrush nbrush,QPoint norigin,int nrx,int nry):
+        Shape(device,nid,s,npen,nbrush),origin(norigin),rx(nrx),ry(nry){}
+    void draw()
+    {
+        QPainter pnt = getPainter();
+        pnt.setPen(this->getPen());
+        pnt.setBrush(this->getBrush());
+        pnt.drawEllipse((getOrigin().x(),(getOrigin()).y(),rx,ry);
+    }
+    void move(QPoint norigin,int nrx,int nry)
+    {
+        setOrigin(norigin);
+        setRx(nrx);
+        setRy(nry);
+    }
+    double calcPerimeter()
+    {
+
+    }
+    double calcArea()
+    {
+
+    }
+    void setOrigin(QPoint norigin)
+    {
+        origin = norigin;
+    }
+    void setRx(int nrx)
+    {
+        rx = nrx;
+    }
+    void setRy(int nry)
+    {
+        ry = nry;
+    }
+    QPoint getOrigin()const
+    {
+        return origin;
+    }
+    int getRx()const
+    {
+        return rx;
+    }
+    int getRy()const
+    {
+        return ry;
+    }
 
 private:
-
+    QPoint origin;
+    int rx, ry;
 };
 
 class Circle : public Ellipse
 {
 public:
+    Circle(QPaintDevice* device,int nid=-1,QPen npen,QBrush nbrush,QPoint norigin,int nr):
+        Ellipse(device,nid,Shape::shape::Circle,npen,nbrush,norigin,nr,nr){}
+    void draw()
+    {
+        this->Ellipse::draw();
+    }
+    void move(QPoint norigin,int nr)
+    {
+        this->Ellipse::move(norigin,nr,nr);
+    }
+    double calcPerimeter()
+    {
 
+    }
+    double calcArea()
+    {
+
+    }
 private:
-
 };
 
 class Text : public Shape
