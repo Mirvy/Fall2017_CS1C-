acc6a47094767e3c2d784d3698c2720fdc2eb40d
yuliyd <yuliy.dubovyk@gmail.com>
2017-11-21 09:25:24 -0800

    Add files via upload

--- feedback.cpp
+++ feedback.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,93 +11,92 @@ struct list
 	string feedback;
 };
 
-void WriteTo(vector <list>& vec);
-void ReadFrom(vector <list>& vec);
-void print(vector <list>& vec);
-
-int main()
-{
-	vector <list> vec;
-	//vector <int> vecInt;
-
-	ReadFrom(vec);
-
-	for(int i=0; i<2; ++i)
-	{
-		list *list1 = new list;
-		cout << "Enter name: ";
-		getline(cin, list1->name);
-
-		cout << "Enter feedback: ";
-		getline(cin, list1->feedback);
-		vec.push_back(*list1);
-		//vecInt.push_back(1);
-	}
-
-	print(vec);
-	WriteTo(vec);
-	return 0;
-}
-
-
-void WriteTo(vector <list>& vec)
-{
-	ofstream oFile;
-	oFile.open("text.txt");
-
-
-	string name = "A man with a mustache";
-	string feedback = "im an oil man and this is my son H.W.";
-
-	/*oFile << '\n';
-	oFile << "User name: " << name;
-	oFile << '\n';
-	oFile << "User feedback: " << feedback;*/
-	//getline(oFile, name);
-
-
-	for(int i=0; i<vec.size(); ++i)
-	{
-		oFile << '\n';
-		oFile << "User name: " << vec[i].name << '\n';
-		oFile << "User feedback: " << vec[i].feedback<< '\n';
-	}
-
-	oFile.close();
-}
-
-void ReadFrom(vector <list>& vec)
+void ReadFrom(vector <list>&vec)
 {
 	ifstream inFile;
 	inFile.open("text.txt");
-	string str;
-	int i = 0;
+	string str ="";
+	int i=0;
 
 	while(!inFile.eof())
 	{
 		list *list1 = new list;
 
 		getline(inFile, str);
-
 		getline(inFile, list1->name);
 		getline(inFile, list1->feedback);
 
 		vec.push_back(*list1);
-
 		++i;
 	}
-
-
 
 	inFile.close();
 }
 
-void print(vector <list>& vec)
+void Print(vector <list>&vec)
 {
-	for(int i = 0; i<vec.size(); ++i)
+	for(unsigned int i= 0; i < vec.size(); ++i)
 	{
 		cout << endl;
 		cout << vec[i].name << endl;
 		cout << vec[i].feedback << endl;
 	}
+}
+
+string promptName()
+{
+	string name;
+	cout << "Name: ";
+	getline(cin, name);
+	return name;
+}
+
+string promptFeedback()
+{
+	string feedback;
+	cout << "Feedback: ";
+	getline(cin, feedback);
+	return feedback;
+}
+
+void GetInfo(vector <list>&vec)
+{
+	list *list1 = new list;
+	string newName = promptName();
+	string newFeedback = promptFeedback();
+
+	list1->name = newName;
+	list1->feedback = newFeedback;
+
+	vec.push_back(*list1);
+}
+
+void WriteTo(vector <list>&vec)
+{
+	ofstream oFile;
+	oFile.open("text.txt");
+
+	for(unsigned int i=0; i<vec.size(); ++i)
+	{
+		oFile << '\n';
+		oFile << "User name: " << vec[i].name << '\n';
+		oFile << "User feedback: " << vec[i].feedback << '\n';
+	}
+	oFile.close();
+}
+
+int main()
+{
+	vector<list> vec;
+	ifstream iFile;
+
+	if(iFile.peek() != iFile.eof())
+	{
+		ReadFrom(vec);
+	}
+
+	GetInfo(vec);
+	Print(vec);
+	WriteTo(vec);
+	return 0;
 }
